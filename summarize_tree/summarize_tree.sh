bb5efddac2c0e3494609eac76fe4a05fa240227d
AndreasNelson <nel02254@morris.umn.edu>
2024-10-21 18:05:11 -0500

    summarize_tree.sh permission fix for failing tests

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -13,4 +13,4 @@ num_regular=$(find "$1" -type f | wc -l)
 
 # Output the results
 echo "There were $((num_dirs - 1)) directories." # Subtract 1 to exclude the base directory itself
-echo "There were $num_regular regular files."
+echo "There were $num_regular regular files." # output the number of regular files
